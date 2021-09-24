82aa11954c2efad46a9a534036a141145572d8b2
ThurberDaniel <phxburg@gmail.com>
2021-09-24 09:30:19 -0600

    update

--- index.py
+++ index.py
@@ -5,8 +5,16 @@ age = 42
 
 todayIsCold = True
 
-if todayIsCold == True:
+if todayIsCold == False:
     print("WIN")
 
 else:
     print("NOPE")
+
+
+year = 1830
+
+if 2000 <= year < 2100:
+    print("welcome to the 21st century")
+else:
+    print("you are before or after the 21st century")
