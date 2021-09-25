5ca8f2f4ab5fc472ddb8592322381b76c95764d4
ThurberDaniel <phxburg@gmail.com>
2021-09-25 09:16:26 -0600

    sat

--- index.py
+++ index.py
@@ -1,20 +1,7 @@
 
-name = "timmy"
 
-age = 42
-
-todayIsCold = True
-
-if todayIsCold == False:
-    print("WIN")
-
-else:
-    print("NOPE")
+def triple(word):
+    print(word*4)
 
 
-year = 1830
-
-if 2000 <= year < 2100:
-    print("welcome to the 21st century")
-else:
-    print("you are before or after the 21st century")
+triple("King")
